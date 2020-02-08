fade05abdcfacbcc2438ec6743c905c02514fe68
uneeb123 <uneeb.i.am@gmail.com>
2020-02-08 14:11:53 -0800

    charts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="chart">Sup</div>
+  <div id="chart"></div>
 </template>
 
 <script>
@@ -7,9 +7,53 @@ import * as d3 from "d3";
 import raceChart from "./raceChart.js";
 export default {
   async created() {
-    const data = await d3.csv("/data/category-brands.csv");
+    const data2 = await d3.csv("/data/category-brands.csv");
+    const cryptoFileToNames = {
+      ada: "Cardano",
+      atom: "Cosmos",
+      bch: "Bitcoin Cash",
+      bnb: "Binance Coin",
+      bsv: "Bitcoin SV",
+      btc: "Bitcoin",
+      dash: "Dash",
+      etc: "Ethereum Classic",
+      eth: "Ethereum",
+      leo: "UNUS SED LEO",
+      link: "Chainlink",
+      ltc: "Litecoin",
+      neo: "Neo",
+      tether: "Tether",
+      trx: "Tron",
+      xlm: "Stellar",
+      xmr: "Monero",
+      xrp: "XRP",
+      xtz: "Tezos"
+    };
+    const marketCap = {};
+    const cryptoFiles = Object.keys(cryptoFileToNames);
+    const cryptoNames = Object.values(cryptoFileToNames);
+    await Promise.all(
+      cryptoFiles.map(async cryptoFile => {
+        const data = await d3.csv(`/data/${cryptoFile}.csv`);
+        marketCap[`${cryptoFileToNames[cryptoFile]}`] = data
+          .map(d => {
+            const date = new Date(d["Date"]);
+            const marketCap = parseInt(d["Market Cap"].split(",").join(""));
+            if (!isNaN(date) && !isNaN(marketCap) && marketCap !== 0) {
+              return {
+                date: date,
+                marketCap: marketCap
+              };
+            } else {
+              return null;
+            }
+          })
+          .filter(d => d !== null)
+          .reverse();
+      })
+    );
     const chartSvg = d3.select("#chart").append("svg");
-    raceChart.init(data, chartSvg);
+    raceChart.init(data2, chartSvg, cryptoNames, marketCap);
   }
 };
 </script>
--- src/components/raceChart.js
+++ src/components/raceChart.js
@@ -1,74 +1,120 @@
 import * as d3 from "d3";
+import * as numeral from "numeral";
 import * as d3Array from "d3-array";
 
 const raceChart = {
-  async init(data, svg) {
+  async init(data, svg, cryptoNames, marketCap) {
     const margin = ({
       top: 16,
       right: 6,
       bottom: 6,
       left: 0
     });
-    const width = 500;
-    const duration = 50
-    const n = 12
-    const formatDate = d3.utcFormat("%Y")
+    const width = 1000;
+    const labelWidth = 200;
+    const duration = 300
+    const n = 10
     const formatNumber = d3.format(",d")
-    const barSize = 20
+    const barSize = 48
     const height = margin.top + barSize * n + margin.bottom
 
+    let dateMap = {};
+    cryptoNames.forEach(crypto => {
+      marketCap[crypto].forEach(val => {
+        const timestamp = new Date(val.date).getTime();
+        if (!(timestamp in dateMap)) {
+          dateMap[timestamp] = {};
+        }
+        dateMap[timestamp][crypto] = val.marketCap;
+      })
+    });
+    let timestamps = Object.keys(dateMap).sort();
+    let dateValues = timestamps.reduce((acc, curr) => {
+      const date = new Date(parseInt(curr));
+      acc.push([date, dateMap[curr]]);
+      return acc;
+    }, []);
 
-    const names = new Set(data.map(d => d.name))
-    const datevalues = Array.from(d3Array.rollup(data, ([d]) => d.value, d => d.date, d => d.name))
-      .map(([date, data]) => [new Date(date), data])
-      .sort(([a], [b]) => d3.ascending(a, b))
 
-    function rank(value) {
-      const data = Array.from(names, name => ({
-        name,
-        value: value(name) || 0
-      }));
-      data.sort((a, b) => d3.descending(a.value, b.value));
-      for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
-      return data;
-    }
-    const k = 10
-
-    const keyframes = [];
-    let ka, a, kb, b;
-    for ([
-        [ka, a],
-        [kb, b]
-      ] of d3.pairs(datevalues)) {
-      for (let i = 0; i < k; ++i) {
-        const t = i / k;
-        keyframes.push([
-          new Date(ka * (1 - t) + kb * t),
-          rank(name => a.get(name) * (1 - t) + b.get(name) * t)
-        ]);
+    function extendKeyframes(dateValues, k) {
+      function rank(value) {
+        const data = Array.from(cryptoNames, name => ({
+          name,
+          value: value(name) || 0
+        }));
+        data.sort((a, b) => b.value - a.value);
+        for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
+        return data;
       }
+
+      const keyframes = [];
+      let ka, a, kb, b;
+      for ([
+          [ka, a],
+          [kb, b]
+        ] of d3.pairs(dateValues)) {
+        for (let i = 0; i < k; ++i) {
+          const t = i / k;
+          keyframes.push([
+            new Date(ka * (1 - t) + kb * t),
+            rank(name => a[name] * (1 - t) + b[name] * t)
+          ]);
+        }
+      }
+      keyframes.push([new Date(kb), rank(name => b[name])]);
+      return keyframes;
     }
-    keyframes.push([new Date(kb), rank(name => b.get(name))]);
+
+    function skipKeyframes(dateValues, k) {
+      const keyframes = [];
+      dateValues.forEach(dateValue => {
+        let toArray = [];
+        cryptoNames.forEach(cryptoName => {
+          toArray.push({
+            name: cryptoName,
+            value: dateValue[1][cryptoName] || 0
+          });
+        })
+        toArray = toArray.filter(element => element.value > 0);
+        toArray.sort((a, b) => b.value - a.value);
+        toArray.forEach((element, i) => {
+          element.rank = i;
+        });
+        keyframes.push([dateValue[0], toArray]);
+      });
+      const skippedFrames = [];
+      for (let i = 0; i < keyframes.length; i++) {
+        if (i % k === 0) {
+          skippedFrames.push(keyframes[i]);
+        }
+      }
+      return skippedFrames;
+    }
+
+    const keyframes = skipKeyframes(dateValues, 10);
 
     const nameframes = d3Array.groups(keyframes.flatMap(([, data]) => data), d => d.name)
 
-    const prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
+    const prev = nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a]))
 
-    const next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
+    const next = nameframes.flatMap(([, data]) => d3.pairs(data))
+
+    const x = d3.scaleLinear().domain([0, 1000000000]).range([margin.left, width - margin.right]);
+
+    const y = d3.scaleBand()
+      .domain(d3.range(n + 1))
+      .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
+      .padding(0.1)
 
     const color = function () {
-      const scale = d3.scaleOrdinal(d3.schemeTableau10);
-      if (data.some(d => d.category !== undefined)) {
-        const categoryByName = new Map(data.map(d => [d.name, d.category]))
-        scale.domain(Array.from(categoryByName.values()));
-        return d => scale(categoryByName.get(d.name));
-      }
+      const scale = d3.scaleOrdinal(d3.schemeCategory10);
       return d => scale(d.name);
     }();
 
     function bars(svg) {
       let bar = svg.append("g")
         .attr("fill-opacity", 0.6)
+        .attr("transform", `translate(${labelWidth},0)`)
         .selectAll("rect");
 
       return ([date, data], transition) => bar = bar
@@ -78,12 +124,12 @@ const raceChart = {
           .attr("fill", color)
           .attr("height", y.bandwidth())
           .attr("x", x(0))
-          .attr("y", d => y((prev.get(d) || d).rank))
-          .attr("width", d => x((prev.get(d) || d).value) - x(0)),
+          .attr("y", d => y((prev[d] || d).rank))
+          .attr("width", d => x((prev[d] || d).value) - x(0)),
           update => update,
           exit => exit.transition(transition).remove()
-          .attr("y", d => y((next.get(d) || d).rank))
-          .attr("width", d => x((next.get(d) || d).value) - x(0))
+          .attr("y", d => y((next[d] || d).rank))
+          .attr("width", d => x((next[d] || d).value) - x(0))
         )
         .call(bar => bar.transition(transition)
           .attr("y", d => y(d.rank))
@@ -101,24 +147,18 @@ const raceChart = {
         .data(data.slice(0, n), d => d.name)
         .join(
           enter => enter.append("text")
-          .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
+          .attr("transform", d => `translate(${labelWidth},${y(d.rank)})`)
           .attr("y", y.bandwidth() / 2)
           .attr("x", -6)
-          .attr("dy", "-0.25em")
-          .text(d => d.name)
-          .call(text => text.append("tspan")
-            .attr("fill-opacity", 0.7)
-            .attr("font-weight", "normal")
-            .attr("x", -6)
-            .attr("dy", "1.15em")),
+          .text(d => d.name),
           update => update,
           exit => exit.transition(transition).remove()
-          .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
-          .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
+          .attr("transform", d => `translate(${labelWidth},${y(d.rank)})`)
+          //.call(g => g.select("tspan").tween("text", d => textTween(d.value, d.value)))
         )
         .call(bar => bar.transition(transition)
-          .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
-          .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
+          .attr("transform", d => `translate(${labelWidth},${y(d.rank)})`))
+      // .call(g => g.select("tspan").tween("text", d => textTween(d.value, d.value))));
     }
 
     function textTween(a, b) {
@@ -130,22 +170,26 @@ const raceChart = {
 
     function axis(svg) {
       const g = svg.append("g")
-        .attr("transform", `translate(0,${margin.top})`);
+        .attr("transform", `translate(${labelWidth},${margin.top})`);
 
       const axis = d3.axisTop(x)
-        .ticks(width / 160)
+        //.ticks(width / 100)
+        .tickFormat(d => numeral(d).format("($0.0a)"))
         .tickSizeOuter(0)
         .tickSizeInner(-barSize * (n + y.padding()));
 
       return (_, transition) => {
         g.transition(transition).call(axis);
         g.select(".tick:first-of-type text").remove();
-        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+        g.selectAll(".tick:not(:first-of-type) line")
+          .attr("stroke", "white")
+          .attr("stroke-width", 0.5);
         g.select(".domain").remove();
       };
     }
 
     function ticker(svg) {
+      const formatDate = d3.utcFormat("%Y")
       const now = svg.append("text")
         .style("font", `bold ${barSize}px var(--sans-serif)`)
         .style("font-variant-numeric", "tabular-nums")
@@ -159,14 +203,6 @@ const raceChart = {
         transition.end().then(() => now.text(formatDate(date)));
       };
     }
-
-    const x = d3.scaleLinear([0, 1], [margin.left, width - margin.right])
-
-    const y = d3.scaleBand()
-      .domain(d3.range(n + 1))
-      .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
-      .padding(0.1)
-
 
     svg.attr("viewBox", [0, 0, width, height]);
 
